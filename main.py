3cbd76b73cfd218f8587f7f087169f6ec6c923f2
crusttacean8 <justinejtorres@gmail.com>
2026-03-13 16:37:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -46,4 +46,5 @@ async def predict(file: UploadFile = File(...)):
         return {"success": False, "error": str(e)}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=10000) # Ensure host is 0.0.0.0 for Render
+    uvicorn.run(app, host="0.0.0.0", port=10000)
+
